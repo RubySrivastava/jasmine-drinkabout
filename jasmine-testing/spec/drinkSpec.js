3bc950725f5dd909fc6f705a3ea0dfdc6b7defe5
RubySrivastava <ruby.srivas1984@gmail.com>
2020-12-03 22:46:30 +0000

    programming defensively

---
+++ jasmine-testing/spec/drinkSpec.js
@@ -0,0 +1,22 @@
+describe("DrinkAbout",function(){
+    describe("whatCanIDrink function",function(){
+        it ("should return Sorry. I can’t tell what drink because that age is incorrect!",function(){
+            expect(whatCanIDrink(-5)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it ("should return Drink Toddy",function(){
+            expect(whatCanIDrink(10)).toBe("Drink Toddy");
+        });
+        it ("should return Drink Toddy",function(){
+            expect(whatCanIDrink(15)).toBe("Drink Coke");
+        });
+        it ("should return Drink Toddy",function(){
+            expect(whatCanIDrink(20)).toBe("Drink Beer");
+        });
+        it ("should return Drink Toddy",function(){
+            expect(whatCanIDrink(100)).toBe("Drink Whisky");
+        });
+        it ("should return Sorry. I can’t tell what drink because that age is incorrect!",function(){
+            expect(whatCanIDrink("abcd")).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+    });
+});
